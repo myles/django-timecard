4828a66820432d777a253eb6f1e631b6dad61c94
Myles Braithwaite <me@mylesbraithwaite.com>
2012-06-12 14:43:10 -0400

    Starting from scratch.

--- src/timecard/urls.py
+++ src/timecard/urls.py
@@ -1,16 +1,4 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('timecard.views',
-	url(r'^(?P<username>[-\w]+)/(?P<year>\d{4})-(?P<week>\d{2})/$',
-		view = 'weekly',
-		name = 'timecard_weekly',
-	),
-	url(r'^(?P<username>[-\w]+)/$',
-		view = 'user',
-		name = 'timecard_user',
-	),
-	url(r'^$',
-		view = 'index',
-		name = 'timecard_homepage',
-	),
 )
