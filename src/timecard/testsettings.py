446e550c3c034eeaac5d92600d3fbd4214af6a52
Myles Braithwaite <me@mylesbraithwaite.com>
2012-06-12 14:45:03 -0400

    Fixing up stuff for DJango 1.4

--- src/timecard/testsettings.py
+++ src/timecard/testsettings.py
@@ -1,8 +1,12 @@
 DEBUG = True
 DEBUG_TEMPLATE = True
 SITE_ID = 1
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/tmp/django-timecard-devel.db'
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': '/tmp/django-timecard-devel.db'
+	}
+}
 INSTALLED_APPS = [
 	'django.contrib.auth',
 	'django.contrib.sessions',
@@ -11,6 +15,7 @@ INSTALLED_APPS = [
 	'django.contrib.admindocs',
 	'django.contrib.comments',
 	'django.contrib.contenttypes',
+	
 	'timecard',
 ]
 ROOT_URLCONF = 'timecard.testurls'
